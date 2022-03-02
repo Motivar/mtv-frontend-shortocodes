1f570d887104eadf7056587f4489e370a303e642
gnnpls <n.giannopoulos@motivar.io>
2022-03-02 10:02:22 +0200

    first commit

---
+++ mtv-frontend-shortcodes.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: AWM WP Analytics
+Plugin URI: https://motivar.io
+Description: Track posts analytics with REST API.
+Version: 1
+Author: Giannopoulos Nikolaos
+Author URI: https://motivar.io
+ */
+
+define('mtv_fr_sh_url', plugin_dir_url(__FILE__));
+define('mtv_fr_sh_path', plugin_dir_path(__FILE__));
+define('mtv_fr_sh_relative_path', dirname(plugin_basename(__FILE__)));
+
+require_once 'includes/init.php';
